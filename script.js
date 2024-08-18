a600b80d0f8668526ec8234dd5e29caa27aae2e3
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-08-18 07:28:36 +0000

    cmvjaoisjg

---
+++ script.js
@@ -0,0 +1,5 @@
+const moodToppings = {
+  'happy': ['pineapple', 'bell peppers', 'ham'],
+  'sad': ['extra cheese', 'mushrooms', 'olives'],
+  'excited': ['pepperoni', 'jalapenos', 'sausage'],
+  'tired': ['spinach', 'feta', 'tomatoes'],
